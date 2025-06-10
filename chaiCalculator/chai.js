44291fb91696ad7c75e9e96162f8f134f988e6ed
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-06-10 23:58:58 +0300

    chai boora user interface

--- chaiCalculator/chai.js
+++ chaiCalculator/chai.js
@@ -1,18 +1,24 @@
 function calculateChaiIngredients() {
-    let input = prompt("Karibu! How many cups ofChai Bora would you like to make?");
-    let numberOfCups = Number(input);
+    let numberOfCups = document.getElementById("cupsInput").value;
+    numberOfCups = Number(numberOfCups);
 
     let totalWater = 200 * numberOfCups;
     let totalMilk = 50 * numberOfCups;
     let totalTeaLeaves   = 1 *  numberOfCups;
     let totalSugar = 2 * numberOfCups;
 
-    console.log(`To make ${numberOfCups} cups of Kenyan Chai, you will need: `);
-    console.log(`Water: ${totalWater} ml`);
-    console.log(`Milk: ${totalMilk} ml`);
-    console.log(`Tea Leaves (Majani): ${totalTeaLeaves} tablespoons`);
-    console.log(`Sugar (Sukari): ${totalSugar} teaspoons`);
-
-    console.log(`Enjoy your Chai Bora!`);
+    document.getElementById("output").innerHTML = `
+     <p>To make ${numberOfCups} cups of Kenyan Chai, you will need: </p>
+     <ul>
+       <li>Water: ${totalWater} ml </li>
+       <li>Milk: ${totalMilk} ml </li>
+       <li>Tea Leaves (Majani): ${totalTeaLeaves} tablespoons </li>
+       <li>Sugar (Sukari): ${totalSugar} teaspoons </li>
+     </ul>
+     
+     <p>Enjoy your Chai Bora! </p>
+    `;
+    
+    
 }
 calculateChaiIngredients();
