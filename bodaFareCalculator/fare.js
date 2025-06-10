472468d737173cd1b8af17fb3b25fccad125a4a2
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-06-11 00:25:29 +0300

    boda user interface

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,18 +1,23 @@
 function calculateBodaFare() {
-    let input = prompt("Unafika wapi Mkubwa? Kilometer ngapi?: ");
-    let distanceInKm = Number(input);
+    let distanceInKm = document.getElementById("kmInput").value;
+    distanceInKm = Number(distanceInKm);
 
     const baseFare = 50;
     const chargePerKm = 15;
     const farecharge = chargePerKm * distanceInKm;
     const totalFare = baseFare + farecharge;
 
-    console.log(`Uko kwote? Io ni ${distanceInKm} km:`);
-    console.log(`Ukikalia Pikipiki : KES ${baseFare}`)
-    console.log(`Mpaka Uko : KES ${farecharge}`);
-    console.log(`Total: KES ${totalFare}`);
+    document.getElementById("output").innerHTML = `
+      <p>Unafika wapi Mkubwa? Kilometer ngapi? : ${distanceInKm} </p>
+      <ul>
+        <li>Uko kwote? Io ni ${distanceInKm} km: </li>
+        <li>Ukikalia Pikipiki : KES ${baseFare} </li>
+        <li>Mpaka Uko : KES ${farecharge} </li>
+        <li>Total: KES ${totalFare} </li>
+      </ul>
+      <p>Panda Pikipiki! </p>
+      
+    `;
+    
 
-    console.log(`Panda Pikipiki!`)
-
-}
-calculateBodaFare();
+};
